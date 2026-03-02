ed3d3b6bc51b58aa3804efae373eff86330775a8
Nacturem <gabriel.grohl2006@gmail.com>
2026-03-02 15:41:22 -0300

    adicionada client de email

---
+++ src/main/java/com/javanauta/bff_agendador/business/EmailService.java
@@ -0,0 +1,22 @@
+package com.javanauta.bff_agendador.business;
+
+
+import com.javanauta.bff_agendador.business.dto.out.TarefasDTOResponse;
+import com.javanauta.bff_agendador.infraescture.client.EmailClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final EmailClient emailClient;
+
+
+    public void enviarEmail (TarefasDTOResponse dto) {
+        emailClient.enviarEmail(dto);
+
+    }
+
+}
+
